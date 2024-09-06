ca4325bbb9b7e6a52beeb69e24b9c73d0907c357
bryceberczik <berczikbryce@gmail.com>
2024-09-06 13:52:43 -0500

    added archive interation, still need to be able to delete archived times as well as fix the styling

--- src/components/archived-times/archivedTimes.jsx
+++ src/components/archived-times/archivedTimes.jsx
@@ -1,4 +1,16 @@
 import './archivedTimes.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+function formatTime(ms) {
+    const totalSeconds = Math.floor(ms / 1000);
+    const seconds = totalSeconds % 60;
+    const milliseconds = Math.floor((ms % 1000) / 10);
+
+    return [
+        seconds.toString().padStart(2, '0'),
+        milliseconds.toString().padStart(2, '0')
+    ].join(':');
+}
 
 export default function ArchivedTimes({ times }) {
     return (
@@ -6,7 +18,10 @@ export default function ArchivedTimes({ times }) {
             <div className="archived-times">
                 {times.map((time, index) => (
                     <div key={index} className="archived-time">
-                        {new Date(time).toISOString().substring(11, 19)}
+                        <span className="archived-time-text">
+                            <FontAwesomeIcon icon={faTrash} className='trash'/>
+                            {formatTime(time)}
+                        </span>
                     </div>
                 ))}
             </div>
